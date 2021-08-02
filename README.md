589bd3f0fa772488e681813bc7beac1b0096e45c
NachoHernandezV <hjoseignacio@hotmail.com>
2021-08-02 11:31:53 -0500

    Codigo del cuadrado

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # curso-practico-javascript
-Curso Practico de JavaScript en Platzi
 
 
-#INICIAMOS EL CURSO 
+#Taller #1: figuras geometricas
+
+--Primer paso: definir las formulas
+--Segundo: Implementar las formulas en JavaScript
+--Tercer paso: crear funciones
+--Cuarto paso: Integrar JS con HTML
---
+++ figuras.js
@@ -0,0 +1,11 @@
+//CODIGO DEL CUADRADO
+const ladoCuadrado=5;
+console.log("Los lados del cuadrado miden : "+ladoCuadrado + " Cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perimetro del cuadrado es : "+ perimetroCuadrado + " Cm");
+
+const areaCuadrada = ladoCuadrado * ladoCuadrado;
+console.log("El Area Cuadrada es : " + areaCuadrada + " Cm2");
+//CODIGO DEL CUADRADO
+
